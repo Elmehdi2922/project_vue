d7035112569e8ab836077f28a1e42b24e1c00327
lagrida <lagyassine@gmail.com>
2020-08-01 22:03:06 +0000

    adding project

---
+++ src/components/AdminSideBar.vue
@@ -0,0 +1,58 @@
+<template>
+                <nav id="sidebar" class="m-0">
+                    <div class="sidebar-header">
+                        <h3>Administration</h3>
+                    </div>
+                    <ul class="list-unstyled components">
+                        <p>Services :</p>
+                        <li class="active">
+                            <router-link to="/admin/payments" class="dropdown-toggle">Payment</router-link>
+                            <ul class="collapse list-unstyled" id="homeSubmenu">
+                                <li>
+                                    <a href="#">Home 1</a>
+                                </li>
+                                <li>
+                                    <a href="#">Home 2</a>
+                                </li>
+                                <li>
+                                    <a href="#">Home 3</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false">Products</a>
+                            <ul class="collapse list-unstyled" id="pageSubmenu">
+                                <li>
+                                    <a href="#">Page 1</a>
+                                </li>
+                                <li>
+                                    <a href="#">Page 2</a>
+                                </li>
+                                <li>
+                                    <a href="#">Page 3</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">Users</a>
+                        </li>
+                        <li>
+                            <a href="#">Carts</a>
+                        </li>
+                    </ul>
+
+                    <ul class="list-unstyled CTAs">
+                        <li>
+                            <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a>
+                        </li>
+                        <li>
+                            <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a>
+                        </li>
+                    </ul>
+                </nav>
+</template>
+<script>
+export default {
+  name: "AdminSideBar"
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Admin from "../views/Admin.vue";
+import Payments from "../views/Payments.vue";
+import GiftCards from "../views/GiftCards.vue";
+import AddGiftCard from "../views/AddGiftCard.vue";
+import updateGiftCard from "../views/updateGiftCard.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +14,31 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/admin",
+    name: "Admin",
+    component: Admin
+  },
+  {
+    path: "/admin/payments",
+    name: "Payments",
+    component: Payments
+  },
+  {
+    path: "/admin/payments/giftcards",
+    name: "GiftCards",
+    component: GiftCards
+  },
+  {
+    path: "/admin/payments/giftcards/add",
+    name: "AddGiftCard",
+    component: AddGiftCard
+  },
+  {
+    path: "/admin/payments/giftcards/update/:id",
+    name: "updateGiftCard",
+    component: updateGiftCard
   },
   {
     path: "/about",
